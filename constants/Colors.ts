d8bb0f93ae700a1d44067179aee11faaffeb552c
Bayode Gabriel <bayogabriel24@gmail.com>
2020-01-04 12:00:00 +0100

    V1.0

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,6 +3,10 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
+
+
+
+
 const tintColorLight = '#0a74';
 const tintColorDark = '#ff';
 
